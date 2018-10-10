d5db166cd4d08567d7c2b0c00ec2a5a244c47642
duongdinhmanh <duongdinhmanh252@gmail.com>
2018-10-10 13:57:10 +0700

    create Migration

---
+++ database/migrations/2018_10_04_085848_create_apartment_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApartmentImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('apartment_id')->unsigned();
+            $table->string('link')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->foreign('apartment_id')->references('id')
+                ->on('apartments')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_images');
+    }
+}
